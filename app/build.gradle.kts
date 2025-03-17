47bdd881003f58a8ce637bacf49baafb378b8c79
Chernuhin-Dmitriy <chernuhin2020@mail.ru>
2025-03-17 11:27:57 +0300

    Prepair ShopItemActivity layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
